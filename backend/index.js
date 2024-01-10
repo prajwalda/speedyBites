efe3cc478dd54fa79b4b09febad2e9e89878ccc4
Prajwal Motghare <52370665+prajwalda@users.noreply.github.com>
2024-01-10 23:04:08 +0530

    Update index.js
    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cors = require('cors');
 
-const mongoDB = require("./db")
-mongoDB()
+const mongoDB = require("./db");
+mongoDB();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello world");
-})
+});
+
+// Enable CORS for all routes
+app.use(cors({ origin: 'http://localhost:3001' }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://speedy-bites.vercel.app/");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
@@ -18,12 +22,11 @@ app.use((req, res, next) => {
     next();
 });
 
-
-app.use(express.json())
-app.use('/api',require("./Routes/CreateUser"))
-app.use('/api',require("./Routes/DisplayData"))
-app.use('/api',require("./Routes/OrderData"))
+app.use(express.json());
+app.use('/api', require("./Routes/CreateUser"));
+app.use('/api', require("./Routes/DisplayData"));
+app.use('/api', require("./Routes/OrderData"));
 
 app.listen(port, () => {
     console.log(`backend is running on port ${port}`);
-})
+});
